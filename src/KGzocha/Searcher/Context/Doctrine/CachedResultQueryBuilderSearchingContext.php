a743c94f038311eb3581892e04d43d6b4b600e40
AbdElKader Bouadjadja <abouadjadja@tradetracker.com>
2016-08-09 21:49:04 +0400

    PHPdoc improvment

--- src/KGzocha/Searcher/Context/Doctrine/CachedResultQueryBuilderSearchingContext.php
+++ src/KGzocha/Searcher/Context/Doctrine/CachedResultQueryBuilderSearchingContext.php
@@ -1,19 +1,11 @@
 <?php
 
-/*
- * This file is part of the searcher package.
- *
- * (c) AbdElKader Bouadjadja <ak.bouadjadja@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 namespace KGzocha\Searcher\Context\Doctrine;
 
 /**
- * Class CachedResultQueryBuilderSearchingContext
+ * Use this class as a SearchingContext in order to allow all criteria builders
+ * to work with Doctrine's QueryBuilder, with result caching enabled by default.
  *
- * @package KGzocha\Searcher\Context\Doctrine
  * @author AbdElKader Bouadjadja <abouadjadja@tradetracker.com>
  */
 class CachedResultQueryBuilderSearchingContext extends QueryBuilderSearchingContext
